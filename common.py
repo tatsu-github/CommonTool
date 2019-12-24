ba8b7bc6db1231692a1578eaed469890732f83fb
tatsu-github <32174880+tatsu-github@users.noreply.github.com>
2019-12-24 14:13:49 +0900

    Update common.py

--- common.py
+++ common.py
@@ -2,4 +2,8 @@
 import mymodule
 mymodule.__file__
 
-
+# Get directory existence and create if not exist
+import os
+path = 'SERVER/project/ep01/sq01/s01/maya/cache'
+if os.path.exists(path) == 0:
+    os.makedirs(path)
